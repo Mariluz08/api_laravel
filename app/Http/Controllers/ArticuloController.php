281aba60bd51850e2becea4e22d42d7ece9e2ad0
mariluz <mariluz14955@gmail.com>
2021-09-14 09:00:43 -0500

    pruebas

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 
 class ArticuloController extends Controller
 {
-    //
+    // estp es una prueba
        public function index()
     {
         return Articulo::all();
